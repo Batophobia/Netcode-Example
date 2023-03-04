f62c1e0de5a5d65402bbf67d66b6a9f02c265e80
batophobia <godrox3@yahoo.com>
2023-03-03 23:56:46 -0600

    Client trusted variables

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 using Unity.Netcode;
 
 public class PlayerNetwork : NetworkBehaviour {
+  NetworkVariable<int> randNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
   void Update() {
+    Debug.Log(OwnerClientId + " - " + randNumber.Value);
     if(!IsOwner) return;
+
+    if(Input.GetKeyDown(KeyCode.T))
+      randNumber.Value = Random.Range(0,100);
 
     Vector3 moveDir = new Vector3(0,0,0);
 
